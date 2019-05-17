a2a42b1b01ec77ac2f3c75a0252af95c96e48a3a
li.xianchun <li.xianchun@vpclub.cn>
2019-05-17 19:55:33 +0800

    ExceptionHandler

--- src/main/java/com/chun/proxy/control/interfaceControl/TestInterfaceControl.java
+++ src/main/java/com/chun/proxy/control/interfaceControl/TestInterfaceControl.java
@@ -1,7 +1,14 @@
 package com.chun.proxy.control.interfaceControl;
 
 import com.chun.proxy.control.request.Req;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 接口control测试类
@@ -10,6 +17,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestInterfaceControl implements ControlInterface {
     @Override
     public String test(Req req) {
+        if (true){
+            throw new NullPointerException();
+        }
         return "uuuuuuuuuuuu";
     }
+
+
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(value = HttpStatus.OK)
+    public Object handleException(Exception exp, HttpServletRequest request, HttpServletResponse response) {
+        if (exp.getMessage() != null){
+            return exp.getMessage();
+        }
+
+      /*  @RestControllerAdvice与@ExceptionHandler注解是sprngmvc中与异常捕获与处理相关的注解，它的入口也是DispatcherServlet中的doDispatcher()方法中，如下：
+        this.processDispatchResult(processedRequest, response, mappedHandler, mv, (Exception)dispatchException);
+
+        org.springframework.web.servlet.DispatcherServlet#processHandlerException
+        org.springframework.web.servlet.handler.AbstractHandlerExceptionResolver#resolveException,实例HandlerExceptionResolverComposite
+       */
+
+      return exp.getMessage();
+    }
+
+
+
 }
